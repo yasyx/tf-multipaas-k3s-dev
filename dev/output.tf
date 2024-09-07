414648e5cd8e52751903e55b852f08f5fd230607
chengzilong <237643381@qq.com>
2024-09-07 15:33:10 +0800

    ~

--- dev/output.tf
+++ dev/output.tf
@@ -1,3 +1,7 @@
 output "how_to_get_argocd_user_and_password" {
   value = "Default user is 'admin' and to exec 'kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d' to get the password."
 }
+
+output "how_to_visit_argocd" {
+    value = "Excute the command `kubectl port-forward svc/argocd-server -n argocd 8080:80` , and visit the url 'http://localhost:8080' via browser."
+}
